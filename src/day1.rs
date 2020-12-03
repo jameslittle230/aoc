712db4b041c5cf5e842250297783c80b61edf2bb
James Little <littleguy23@gmail.com>
2020-12-03 13:15:49 -0500

    Days 1 and 2

---
+++ src/day1.rs
@@ -0,0 +1,103 @@
+use std::{
+    error::Error,
+    io::{self, Read},
+    process::exit,
+};
+
+#[allow(dead_code)] 
+fn find_pair(numbers: &Vec<u32>, target: u32) -> Result<(u32, u32), Box<dyn Error>> {
+    for number in numbers.into_iter() {
+        if numbers.contains(&(2020 - number)) {
+            return Ok((number.to_owned(), target - number));
+        }
+    }
+
+    Err(From::from("No pair"))
+}
+
+#[allow(dead_code)] 
+fn find_triplet(numbers: &Vec<u32>, target: u32) -> Result<(u32, u32, u32), Box<dyn Error>> {
+    for number in numbers.into_iter() {
+        if let Ok(pair) = find_pair(numbers, target - number) {
+            return Ok((number.to_owned(), pair.0, pair.1));
+        }
+    }
+
+    Err(From::from("No triplet"))
+}
+
+fn find_n_entries_summing_to(
+    numbers: &Vec<u32>,
+    n: u8,
+    target: u32,
+) -> Result<Vec<u32>, Box<dyn Error>> {
+    match n {
+        1 => {
+            if numbers.contains(&target) {
+                return Ok(vec![target]);
+            } else {
+                return Err(From::from("No"));
+            }
+        }
+
+        _ => {
+            for (index, number) in numbers.into_iter().enumerate() {
+                let mut vec_without_this_number = numbers.clone();
+                vec_without_this_number.remove(index);
+
+                let new_n_value = n
+                    .checked_sub(1)
+                    .ok_or::<Box<dyn Error>>(From::from("n too small"))?;
+
+                let new_target_value = target
+                    .checked_sub(number.to_owned())
+                    .ok_or::<Box<dyn Error>>(From::from("target too small"))?;
+
+                if let Ok(recursive_result) = find_n_entries_summing_to(
+                    &vec_without_this_number,
+                    new_n_value,
+                    new_target_value,
+                ) {
+                    let mut result_so_far = recursive_result.clone();
+                    result_so_far.push(number.clone());
+                    return Ok(result_so_far);
+                }
+            }
+
+            return Err(From::from("Result not found"));
+        }
+    }
+}
+
+pub fn main() {
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer).unwrap();
+
+    let mut numbers: Vec<u32> = buffer
+        .split('\n')
+        .into_iter()
+        .map(|str| str.trim().parse().unwrap())
+        .collect();
+
+    &numbers.sort();
+
+    let entries = find_n_entries_summing_to(&numbers, 3, 2020).unwrap();
+    let mut product = 1;
+    for entry in &entries {
+        product *= entry;
+    }
+
+    eprintln!(
+        "{} = {}",
+        &entries
+            .into_iter()
+            .map(|val| val.to_string())
+            .collect::<Vec<String>>()
+            .join(" × "),
+        product
+    );
+
+    println!("{}", product);
+
+    exit(0)
+}
---
+++ src/day2.rs
@@ -0,0 +1,106 @@
+use std::{
+    convert::TryFrom,
+    convert::TryInto,
+    error::Error,
+    io::{self, Read},
+};
+
+struct PasswordPolicy {
+    letter: char,
+    min: u8,
+    max: u8,
+}
+
+impl TryFrom<String> for PasswordPolicy {
+    type Error = Box<dyn Error>;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        let split: Vec<&str> = value
+            .split(|c| c == '-' || c == ' ')
+            .map(|s| s.trim())
+            .collect();
+
+        if split.len() != 3 {
+            return Err(From::from("Password policy formatted incorrectly"));
+        }
+
+        let min: u8 = split.get(0).unwrap().parse()?;
+        let max: u8 = split.get(1).unwrap().parse()?;
+        let letter: char = split.get(2).unwrap().parse()?;
+
+        Ok(Self { min, max, letter })
+    }
+}
+
+struct Password {
+    policy: PasswordPolicy,
+    password: String,
+}
+
+impl TryFrom<String> for Password {
+    type Error = Box<dyn Error>;
+    fn try_from(string: String) -> Result<Self, Self::Error> {
+        let split: Vec<&str> = string.split(":").map(|s| s.trim()).collect();
+
+        if split.len() != 2 {
+            return Err(From::from("Password entry formatted incorrectly"));
+        }
+
+        let policy = PasswordPolicy::try_from(split.get(0).unwrap().to_string())?;
+        Ok(Self {
+            policy,
+            password: split.get(1).unwrap().to_string(),
+        })
+    }
+}
+
+impl Password {
+    #[allow(dead_code)] 
+    fn is_valid_for_sled_shop(&self) -> bool {
+        let policy_letter_count = self
+            .password
+            .chars()
+            .into_iter()
+            .map(|char| char == self.policy.letter)
+            .map(|bool| u8::from(bool))
+            .sum::<u8>();
+
+        (self.policy.min..=self.policy.max).contains(&policy_letter_count)
+    }
+
+    fn is_valid_for_toboggan(&self) -> bool {
+        let first_position_letter = self.password.chars().nth((self.policy.min - 1) as usize);
+        let second_position_letter = self.password.chars().nth((self.policy.max - 1) as usize);
+
+        match (first_position_letter, second_position_letter) {
+            (Some(first), Some(second)) => {
+                let letter_one_should_match = self.policy.letter;
+                (first == letter_one_should_match) ^ (second == letter_one_should_match)
+            }
+            _ => false,
+        }
+    }
+}
+
+pub fn main() {
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer).unwrap();
+
+    let count: u32 = buffer
+        .split('\n')
+        .into_iter()
+        // map to password
+        .map(|str| str.trim().to_string().try_into().unwrap())
+        // validate password
+        .map(|pw: Password| pw.is_valid_for_toboggan())
+        // map to 1 or 0 to allow for sum
+        .map(|bool| u32::from(bool))
+        .sum::<u32>();
+
+    eprintln!(
+        "{} out of {}",
+        count,
+        buffer.split("\n").collect::<Vec<&str>>().len()
+    );
+    println!("{}", count)
+}
