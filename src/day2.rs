f9cdb4ef1081bf0170c55417d590c37c148eaf67
James Little <littleguy23@gmail.com>
2020-12-04 09:47:33 -0500

    cargo doc

--- src/day2.rs
+++ src/day2.rs
@@ -1,6 +1,96 @@
 use crate::{AOCOutput, Variant};
 use std::{convert::TryFrom, convert::TryInto, error::Error};
 
+/**
+# Part One
+
+Your flight departs in a few days from the coastal airport; the easiest way
+down to the coast from here is via toboggan.
+The shopkeeper at the North Pole Toboggan Rental Shop is having a bad day.
+"Something's wrong with our computers; we can't log in!" You ask if you can
+take a look.
+ 
+Their password database seems to be a little corrupted: some of the
+passwords wouldn't have been allowed by the Official Toboggan Corporate
+Policy that was in effect when they were chosen.
+ 
+To try to debug the problem, they have created a list (your puzzle input) of
+passwords (according to the corrupted database) and the corporate policy
+when that password was set.
+ 
+For example, suppose you have the following list:
+ 
+- 1-3 a: abcde 
+- 1-3 b: cdefg 
+- 2-9 c: ccccccccc 
+
+Each line gives the password
+policy and then the password. The password policy indicates the lowest and
+highest number of times a given letter must appear for the password to be
+valid. For example, 1-3 a means that the password must contain a at least 1
+time and at most 3 times.
+ 
+In the above example, 2 passwords are valid. The middle password, cdefg, is
+not; it contains no instances of b, but needs at least 1. The first and
+third passwords are valid: they contain one a or nine c, both within the
+limits of their respective policies.
+ 
+How many passwords are valid according to their policies?
+ 
+# Part Two
+
+While it appears you validated the passwords correctly, they don't seem to
+be what the Official Toboggan Corporate Authentication System is expecting.
+ 
+The shopkeeper suddenly realizes that he just accidentally explained the
+password policy rules from his old job at the sled rental place down the
+street! The Official Toboggan Corporate Policy actually works a little
+differently.
+ 
+Each policy actually describes two positions in the password, where 1 means
+the first character, 2 means the second character, and so on. (Be careful;
+Toboggan Corporate Policies have no concept of "index zero"!) Exactly one of
+these positions must contain the given letter. Other occurrences of the
+letter are irrelevant for the purposes of policy enforcement.
+ 
+Given the same example list from above:
+ 
+- 1-3 a: abcde is valid: position 1 contains a and position 3 does not. 
+- 1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b. 
+- 2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c. 
+
+How many passwords are valid according to the new interpretation of the 
+policies?
+*/
+pub fn main(buffer: &String, variant: Variant) -> Result<AOCOutput, Box<dyn Error>> {
+    let count: u32 = buffer
+        .split('\n')
+        .into_iter()
+        
+        // map to password
+        .map(|str| str.trim().to_string().try_into().unwrap())
+       
+        // validate password
+        .map(|pw: Password| match variant {
+            Variant::One => pw.is_valid_for_sled_shop(),
+            Variant::Two => pw.is_valid_for_toboggan(),
+        })
+      
+        // map to 1 or 0 to allow for sum
+        .map(|bool| u32::from(bool))
+        .sum::<u32>();
+
+    let stderr = format!(
+        "{} out of {}",
+        count,
+        buffer.split("\n").collect::<Vec<&str>>().len()
+    );
+
+    let stdout = format!("{}", count);
+
+    Ok(AOCOutput { stderr, stdout })
+}
+
 struct PasswordPolicy {
     letter: char,
     min: u8,
@@ -75,33 +165,4 @@ impl Password {
             _ => false,
         }
     }
-}
-
-pub fn main(buffer: &String, variant: Variant) -> Result<AOCOutput, Box<dyn Error>> {
-    let count: u32 = buffer
-        .split('\n')
-        .into_iter()
-        
-        // map to password
-        .map(|str| str.trim().to_string().try_into().unwrap())
-       
-        // validate password
-        .map(|pw: Password| match variant {
-            Variant::One => pw.is_valid_for_sled_shop(),
-            Variant::Two => pw.is_valid_for_toboggan(),
-        })
-      
-        // map to 1 or 0 to allow for sum
-        .map(|bool| u32::from(bool))
-        .sum::<u32>();
-
-    let stderr = format!(
-        "{} out of {}",
-        count,
-        buffer.split("\n").collect::<Vec<&str>>().len()
-    );
-
-    let stdout = format!("{}", count);
-
-    Ok(AOCOutput { stderr, stdout })
 }
--- src/main.rs
+++ src/main.rs
@@ -24,23 +24,28 @@ pub type OperationOutput = Result<AOCOutput, Box<dyn Error>>;
 pub type Operation = fn(&String, Variant) -> OperationOutput;
 
 fn main() {
-    let matches = App::new("AOC 2020")
+    let matches = App::new("Advent of Code")
+        .version("2020")
+        .author("James Little")
+        .about("https://adventofcode.com")
+        .arg(Arg::new("day").short('d').required(true).takes_value(true))
         .arg(
-            Arg::with_name("day")
-                .short("d")
-                .required(true)
-                .takes_value(true),
+            Arg::new("variant")
+                .short('v')
+                .takes_value(true)
+                .default_value("2")
+                .possible_values(&["1", "2"]),
         )
-        .arg(Arg::with_name("variant").short("v").takes_value(true))
         .get_matches();
 
-    let variant = matches.value_of("variant");
-    let operation_result: OperationOutput = match matches.value_of("day").unwrap() {
+    let variant = matches.value_of("variant").unwrap();
+    let day = matches.value_of("day").unwrap();
+    let operation_result: OperationOutput = match day {
         "1" => execute_puzzle("./inputs/1.txt", day1::main, variant),
         "2" => execute_puzzle("./inputs/2.txt", day2::main, variant),
         "3" => execute_puzzle("./inputs/3.txt", day3::main, variant),
         _ => {
-            eprintln!("Day not recognized");
+            eprintln!("Day `{}` not recognized", day);
             exit(1);
         }
     };
@@ -59,19 +64,16 @@ fn main() {
     }
 }
 
-fn execute_puzzle(path: &str, function: Operation, variant: Option<&str>) -> OperationOutput {
+fn execute_puzzle(path: &str, function: Operation, variant: &str) -> OperationOutput {
     let mut buffer = String::new();
     let file = File::open(path)?;
     let mut reader = BufReader::new(file);
     reader.read_to_string(&mut buffer)?;
 
     let variant = match variant {
-        None => Variant::Two,
-        Some(value) => match value {
-            "1" => Variant::One,
-            "2" => Variant::Two,
-            _ => return Err(From::from("Variant not recognized")),
-        },
+        "1" => Variant::One,
+        "2" => Variant::Two,
+        _ => return Err(From::from(format!("Variant `{}` not recognized", variant))),
     };
 
     function(&buffer, variant)
